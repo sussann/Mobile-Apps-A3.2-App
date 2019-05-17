64420b6432e6e762a8fce6011366ac89ce3b6e5f
T-Marsh <thomassjmarsh@gmail.com>
2019-05-17 17:36:24 +1000

    Initial commit

---
+++ app/src/main/java/assistant/genuinecoder/s_assistant/main/SplashScreen.java
@@ -0,0 +1,15 @@
+package assistant.genuinecoder.s_assistant.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import assistant.genuinecoder.s_assistant.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
