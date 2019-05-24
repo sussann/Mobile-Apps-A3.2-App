f230092c37b0be4be2f684b0c8bc0378bf87ea48
T-Marsh <thomassjmarsh@gmail.com>
2019-05-24 14:30:11 +1000

    finished creating working splash screen

--- app/src/main/java/assistant/genuinecoder/s_assistant/main/SplashScreenActivity.java
+++ app/src/main/java/assistant/genuinecoder/s_assistant/main/SplashScreenActivity.java
@@ -1,15 +1,44 @@
 package assistant.genuinecoder.s_assistant.main;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import assistant.genuinecoder.s_assistant.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    private int SLEEP_TIMER = 4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        LogoLauncher logoLauncher =  new LogoLauncher();
+        logoLauncher.start();
+    }
+
+    private class LogoLauncher extends Thread {
+        public void run() {
+            try {
+                sleep(1000 * SLEEP_TIMER);
+            } catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Intent intent = new Intent(SplashScreenActivity.this, AppBase.class);
+            startActivity(intent);
+            SplashScreenActivity.this.finish();
+        }
     }
 }
+
+
